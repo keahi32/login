22b6837691e7e1c448f96024fd1d2828056dab6a
keahi32 <50500100+keahi32@users.noreply.github.com>
2023-02-20 15:30:37 +0100

    Update authenticated.php

--- authenticated.php
+++ authenticated.php
@@ -1,12 +1,9 @@
 <?php
 session_start();
+
+include ('config.php');
 // Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'docker';
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+$con=$conn;
 if ( mysqli_connect_errno() ) {
 	// If there is an error with the connection, stop the script and display the error.
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
